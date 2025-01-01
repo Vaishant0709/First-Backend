242ec8ca60ff72cea26f75f23c7e95ff8b51e7e0
Vaishant <sharma.vaishant@gmail.com>
2025-01-02 02:15:31 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -2,12 +2,26 @@
 // console.log(process.env)
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
 dotenv.config({
   path:'./env'
 });
 
 connectDB()
+.then(()=>{
+  app.on('error',(error)=>{
+    console.log("EXPRESS CANT CONNECT TO DB :: index.js::   ",error);
+    
+  })
+  app.listen(process.env.PORT || 8000 ,()=>{
+    console.log(`SERVER IS RUNNING AT PORT ${process.env.PORT}`);
+    
+  })
+})
+.catch((error)=>{
+  console.log("MONGO DB CONNECTION FAILED :: index.js ::",error);
+  
+})
 
 
 
